97cf6859bee810ed2496971945dae46472c9a245
GuidoVarga <guidovargamartinez123@gmail.com>
2018-11-06 11:32:43 -0300

    m

--- config/Config.php
+++ config/Config.php
@@ -6,9 +6,9 @@
 		define('DB_NAME', "go_to_event");
 		define('USER',"root");
 		define('PASS',"123-escapa");
-		define('HOST',"localhost");
+		define('HOST',"");
 		define('HOST_INTERNET',"gotoevent.localtest.me");
-		define('DIRECTORY',"GoToEvent");
+		define('DIRECTORY',"GoToSecret");
 		define('PROD-DIRECTORY',"GoToSecret-master");
 		// AIzaSyD2W8ZvnILLOm3N8bLzyTNKpFMYjRkZSL4 Api Google
 		//AIzaSyAusj-vvo9yoWzHao3T6vlcMPoyXyQpXxA Api GeoLocalizacion
