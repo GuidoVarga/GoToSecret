77b79da2ff8de174f366548deb3b366eabb86ebb
Franco Fernandez <franpiussi@gmail.com>
2019-02-07 09:55:57 -0300

    mis pedidos

--- views/user/profile_user.php
+++ views/user/profile_user.php
@@ -59,7 +59,7 @@
 <hr>*/  ?>
 
 <body class="profile_user">
-<div class="container">
+  <div class="container">
     <div class="row my-2">
         <div class="col-lg-12 order-lg-2">
             <ul class="nav nav-tabs">
@@ -67,7 +67,7 @@
                     <a href="" data-target="#profile" data-toggle="tab" class="nav-link active">Mi Perfil</a>
                 </li>
                 <li class="nav-item">
-                    <a href="" data-target="#messages" data-toggle="tab" class="nav-link">Pedidos</a>
+                    <a href="" data-target="#orders" data-toggle="tab" class="nav-link">Pedidos</a>
                 </li>
               
             </ul>
@@ -113,58 +113,77 @@
                             </div>
                         </form>
                 </div>
-                <div class="tab-pane" id="messages">
+                <div class="tab-pane" id="orders">
                     <!--div class="alert alert-info alert-dismissable">
                         <a class="panel-close close" data-dismiss="alert">×</a> This is an <strong>.alert</strong>. Use this to show important messages to the user.
                     </div-->
-                    <table class="table table-hover table-striped">
-                      <thead>
-                        <th>
-                            N° Pedido
-                        </th> 
-                        <th>
-                            Fecha
-                        </th>
-                      </thead>
-                        <tbody>   
-                            <tr data-toggle="collapse" data-target="#accordion1" class="clickable">
-                              <td>
-                                10001
-                              </td>
-                              <td>
-                              19/08/2019
-                              </td>
-                            </tr>
-                            <tr>
+                    <div class="table d-table">
+                      <table class="table table-hover table-striped">
+                          <tbody>   
+                                                              
+                              <tr data-toggle="collapse" data-target="#accordion1" class="clickable collapse-row collapsed">
+                                <td>
+                                    <span class="float-right font-weight-bold">19/08/2019</span> 1000
+                                </td> 
+                              </tr>
+                              <tr>
                                 <td colspan="3">
-                                  <div id="accordion1" class="collapse">
-                                      Evento: Ettap Kyle <br>
-                                      Precio: $1000
-                                  </div>
+                                  <table class="table" id="accordion1" class="collapse">
+                                    <thead>
+                                      <th>
+                                        Evento
+                                      </th>
+                                      <th>
+                                        Precio 
+                                      </th>
+                                    </thead>
+                                    <tbody>
+                                    <td>
+                                      Los Guaranies
+                                    </td>
+                                    <td>
+                                      $1000
+                                    </td>
+                                    </tbody>
+                                  </table>
                                 </td>
-                            </tr>
-                            <tr data-toggle="collapse" data-target="#accordion2" class="clickable">
-                              <td>
-                                10001
-                              </td>
-                              <td>
-                              19/08/2019
-                              </td>
-                            </tr>
-                            <tr>
+                              </tr>
+                              <tr data-toggle="collapse" data-target="#accordion2" class="clickable collapse-row collapsed">
+                                <td>
+                                    <span class="float-right font-weight-bold">20/08/2019</span> 1100
+                                </td>
+                              </tr>
+                              <tr>
                                 <td colspan="3">
-                                  <div id="accordion2" class="collapse">
-                                      Evento: Ettap Kyle <br>
-                                      Precio: $1000
-                                  </div>
+                                  <table class="table" id="accordion2" class="collapse">
+                                    <thead>
+                                      <th>
+                                        Evento
+                                      </th>
+                                      <th>
+                                        Precio 
+                                      </th>
+                                    </thead>
+                                    <tbody>
+                                    <td>
+                                      Ettap Kyle
+                                    </td>
+                                    <td>
+                                      $1000
+                                    </td>
+                                    </tbody>
+                                  </table>
                                 </td>
-                            </tr>
-                        </tbody> 
-                    </table>
+                              </tr>
+                          </tbody> 
+                      </table>
+                      
+                    
+                         
+                    </div> 
                 </div> 
             </div>
-        </div>
-        
+        </div>    
     </div>
-</div>
+  </div>
 </body>
